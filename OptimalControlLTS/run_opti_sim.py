72360c9b8dad425f17e0e31fd9588c669f842190
ananth-shanmugam <ananths36@gmail.com>
2024-11-21 22:01:05 +0000

    doing the bare minimum - some inertia before starting with a car lap sim

--- OptimalControlLTS/run_opti_sim.py
+++ OptimalControlLTS/run_opti_sim.py
@@ -1,12 +1,18 @@
-# Generic Optimal Control Sim
+#%% Generic Optimal Control Sim
 
+# Model Physics
 import src.model.RocketLanding as RocketLanding
 
+# Transcription
 import src.tools.OptiProblem as OptiProblem
-import numpy as np
 
+# Post Processing
+import src.tools.SimOutputs as SimOutputs
+
+# For Visualisation
 import matplotlib.pyplot as plt
 
+#%% Generic Optimal Control Sim
 
 modelFun = RocketLanding.model.factory()
 
@@ -16,20 +22,18 @@ optiProblem, Xs, Us, Gs = OptiProblem.createOptiProblem(modelFun)
 optiProblem.solver('ipopt')
 sol = optiProblem.solve() 
 
-#%%
-x_opt = np.array(optiProblem.value(Xs))
-u_opt = np.array(optiProblem.value(Us))
-g_opt = np.array(optiProblem.value(Gs))
+#%% Assigning Values to Dict
 
-#%%
+SimOut = SimOutputs.createOutputDict(optiProblem, modelFun, Xs, Us, Gs)
 
-# Create two subplots and unpack the output array immediately
+#%% Plots
+
 f, (ax1, ax2, ax3) = plt.subplots(3, 1, sharex=True)
-ax1.plot( modelFun.mesh_points , x_opt[0,:], '-o')
+ax1.plot( modelFun.mesh_points , SimOut.states["velocity"], '-o')
 ax1.set_title('Velocity Trajectory')
 
-ax2.plot( modelFun.mesh_points , x_opt[1,:], '-o')
+ax2.plot( modelFun.mesh_points , SimOut.states["mass"], '-o')
 ax2.set_title('Mass State Trajectory')
 
-ax3.plot( modelFun.mesh_points , u_opt, '-o')
+ax3.plot( modelFun.mesh_points , SimOut.controls["thrust"], '-o')
 ax3.set_title('Thrust Control Trajectory')
